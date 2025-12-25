4d4bb046485829fef518bbbe4d4272d0b10a6519
IamWaqasMuhammad <waqas.developer5254@gmail.com>
2025-12-25 19:38:59 +0500

    add riverpod packages

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:riverpod_practice/screens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
+      home: HomeScreen(),
     );
   }
 }
-
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Screen'),
+        backgroundColor: Colors.indigo,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [Center(child: Text('Home Screen'))],
+      ),
+    );
+  }
+}
